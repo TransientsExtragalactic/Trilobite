b0874a0c5ae4c5349b821a81e7a1cddedadd1930
Eliza Diggins <eliza.diggins@utah.edu>
2026-03-26 20:03:51 -0700

    fix: Ensured all opacity files are in the git repo.

---
+++ triceratops/radiation/opacity/models/__init__.py
@@ -0,0 +1,29 @@
+"""Concrete opacity law implementations.
+
+Classes are defined in :mod:`.core`; this module re-exports them for
+convenience.  See :mod:`.core` for full docstrings and examples.
+"""
+
+from .core import (
+    KAPPA_BF_0,
+    KAPPA_FF_0,
+    KAPPA_KR_0,
+    ConstantOpacity,
+    ElectronScatteringOpacity,
+    KramersBFOpacity,
+    KramersFFOpacity,
+    KramersOpacity,
+)
+
+__all__ = [
+    # Constants
+    "KAPPA_FF_0",
+    "KAPPA_BF_0",
+    "KAPPA_KR_0",
+    # Classes
+    "ConstantOpacity",
+    "ElectronScatteringOpacity",
+    "KramersFFOpacity",
+    "KramersBFOpacity",
+    "KramersOpacity",
+]
